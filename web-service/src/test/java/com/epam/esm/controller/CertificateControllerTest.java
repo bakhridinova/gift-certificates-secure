83be8db15713da99537810556b665cd183909e95
bakhridinova <bakhridinovaleyla@gmail.com>
2023-05-11 22:21:07 +0500

    added test classes

---
+++ web-service/src/test/java/com/epam/esm/controller/CertificateControllerTest.java
@@ -0,0 +1,7 @@
+package com.epam.esm.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CertificateControllerTest {
+
+}
---
+++ web-service/src/test/java/com/epam/esm/repository/OrderRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.epam.esm.repository;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class OrderRepositoryTest {
+
+}
