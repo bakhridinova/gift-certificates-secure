3bbc3973b2c6d2cb2edf66ff716a58d9e96f2175
bakhridinova <bakhridinovaleyla@gmail.com>
2023-05-14 15:40:16 +0500

    removed stereotype repository annotations

--- web-service/src/main/java/com/epam/esm/repository/OrderRepository.java
+++ web-service/src/main/java/com/epam/esm/repository/OrderRepository.java
@@ -7,9 +7,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface OrderRepository extends JpaRepository<Order, Long> {
     @Modifying
     @Query(nativeQuery = true,
