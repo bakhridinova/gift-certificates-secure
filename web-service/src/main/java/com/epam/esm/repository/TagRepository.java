980513614b3c444ced92251654501ca34d7f4dde
bakhridinova <bakhridinovaleyla@gmail.com>
2023-05-14 15:40:16 +0500

    removed stereotype repository annotations

--- web-service/src/main/java/com/epam/esm/repository/TagRepository.java
+++ web-service/src/main/java/com/epam/esm/repository/TagRepository.java
@@ -3,11 +3,9 @@ package com.epam.esm.repository;
 import com.epam.esm.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
-@Repository
 public interface TagRepository extends JpaRepository<Tag, Long> {
     Optional<Tag> findByName(String name);
 
