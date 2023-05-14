3e3f6b2fc441cf3069bc2eecda4e5dcdb45afd2b
bakhridinova <bakhridinovaleyla@gmail.com>
2023-05-14 18:55:56 +0500

    added custom password authentication

---
+++ web-service/src/main/java/com/epam/esm/security/authentication/manager/CustomAuthenticationManager.java
@@ -0,0 +1,29 @@
+package com.epam.esm.security.authentication.manager;
+
+import com.epam.esm.security.authentication.exception.CustomAuthenticationUnsupportedException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author bakhridinova
+ */
+
+@Component
+@RequiredArgsConstructor
+public class CustomAuthenticationManager implements AuthenticationManager {
+    private final List<AuthenticationProvider> authenticationProviders;
+
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return authenticationProviders.stream()
+                .filter(authenticationProvider -> authenticationProvider.supports(authentication.getClass()))
+                .findAny().orElseThrow(() -> new CustomAuthenticationUnsupportedException(authentication.getClass()))
+                .authenticate(authentication);
+    }
+}
---
+++ web-service/src/main/java/com/epam/esm/security/authentication/userdetails/CustomUserDetails.java
@@ -0,0 +1,52 @@
+package com.epam.esm.security.authentication.userdetails;
+
+import com.epam.esm.entity.User;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+/**
+ * @author bakhridinova
+ */
+
+@RequiredArgsConstructor
+public class CustomUserDetails implements UserDetails {
+    private final User user;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return user.getRole().getAuthorities();
+    }
+
+    @Override
+    public String getPassword() {
+        return user.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return user.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
