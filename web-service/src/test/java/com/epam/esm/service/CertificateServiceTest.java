894f504a814037d0ad6d6fd9729776e8cc9e6e5b
bakhridinova <bakhridinovaleyla@gmail.com>
2023-05-17 22:47:21 +0500

    added test set up and context load tests

--- web-service/src/test/java/com/epam/esm/service/CertificateServiceTest.java
+++ web-service/src/test/java/com/epam/esm/service/CertificateServiceTest.java
@@ -1,7 +1,39 @@
 package com.epam.esm.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+import com.epam.esm.dto.CertificateDto;
+import com.epam.esm.repository.CertificateRepository;
+import com.epam.esm.repository.TagRepository;
+import com.epam.esm.service.impl.CertificateServiceImpl;
+import com.epam.esm.util.hateoas.HateoasAdder;
+import com.epam.esm.util.mapper.CertificateMapper;
+import com.epam.esm.util.mapper.TagMapper;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
 
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@ExtendWith(SpringExtension.class)
 class CertificateServiceTest {
+    @Mock
+    private CertificateRepository certificateRepository;
+    @Mock
+    private CertificateMapper certificateMapper;
+    @Mock
+    private TagRepository tagRepository;
+    @Mock
+    private TagMapper tagMapper;
 
+    @Mock
+    private HateoasAdder<CertificateDto> certificateHateoasAdder;
+
+    @InjectMocks
+    private CertificateServiceImpl certificateService;
+
+    @Test
+    void contextLoads() {
+        assertNotNull(certificateService);
+    }
 }
